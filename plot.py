d77183e961834f23da020f02fa4ea23a6b298286
Eduardo Timm Buss <eduardotimmbus@gmail.com>
2025-07-04 15:19:51 -0300

    Update plot.py

--- plot.py
+++ plot.py
@@ -1,18 +1,26 @@
 import matplotlib.pyplot as plt
 
 def plot_training(history):
+    epochs = list(range(len(history['train_loss'])))
+
     plt.figure(figsize=(12, 5))
 
     plt.subplot(1, 2, 1)
-    plt.plot(history['train_loss'], label='Train Loss')
-    plt.plot(history['val_loss'], label='Val Loss')
+    plt.plot(epochs, history['train_loss'], label='Train Loss')
+    plt.plot(epochs, history['val_loss'], label='Val Loss')
     plt.legend()
     plt.title('Loss over Epochs')
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    plt.xlim(left=0)
 
     plt.subplot(1, 2, 2)
-    plt.plot(history['val_acc'], label='Val Accuracy')
+    plt.plot(epochs, history['val_acc'], label='Val Accuracy')
     plt.legend()
     plt.title('Validation Accuracy')
+    plt.xlabel('Epoch')
+    plt.ylabel('Accuracy')
+    plt.xlim(left=0)
 
     plt.tight_layout()
     plt.savefig("training_plot.png")
