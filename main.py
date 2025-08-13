99b064911f1dcb7a806eec0d30e4216b6ccf2bb4
Eduardo Timm Buss <eduardotimmbus@gmail.com>
2025-08-13 12:31:22 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 import torch
-from model import CNNModel
+from model import LightCNNImproved, train_model 
 from data import load_data
-from train import train_model
 
 def main():
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     print(f"Usando dispositivo: {device}")
 
     train_loader, test_loader = load_data()
-    model = CNNModel().to(device)
+
+    model = LightCNNImproved().to(device)
 
     train_model(model, train_loader, test_loader, device)
 
 if __name__ == '__main__':
+    import torch.multiprocessing
+    torch.multiprocessing.set_start_method('spawn', force=True)
     main()
